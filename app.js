9619fe65633084806999f573f88440f044d1fac7
Blvckstone <mohammadshoiab9011@gmail.com>
2023-03-07 22:53:07 +0530

    List Items Adding Complete by Add button in real time

--- app.js
+++ app.js
@@ -4,32 +4,56 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+let itemsArray = ["Reading", "Swimming", "Riding",];
+
 
 app.get("/", function(req, res){
 
-    let today = new  Date();
-    let currentDay = today.getDay();
-    let dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = '';
-
-    if(currentDay === 6 || currentDay === 0){
-        day = dayArray[currentDay];
-    }else{
-        day = dayArray[currentDay]
+    let today = new  Date();
+    // let currentDay = today.getDay();
+    
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
     }
 
 
-
-    res.render("list", {kindOfDay: day})
-
-
-
-
-
-
-    
+    day = today.toLocaleDateString("en-US", options)
+    res.render("list", {kindOfDay: day, itemShow: itemsArray})
+ 
 })
 
 
+app.post("/", function(req, res){
+    itemsArray.push(req.body.newItem)
+    res.redirect('/')
+})
+
 
 app.listen(3000, function(){console.log('Server Started!')})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+
+    // if(currentDay === 6 || currentDay === 0){
+        // day = dayArray[currentDay];
+    // }else{
+        // day = dayArray[currentDay]
+    // }
