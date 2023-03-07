58e4741d465b073e032db1fc5b19886c1122bace
Blvckstone <mohammadshoiab9011@gmail.com>
2023-03-07 13:03:56 +0530

    Day Name With An Array

--- app.js
+++ app.js
@@ -2,9 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req, res){
+
+    let today = new  Date();
+    let currentDay = today.getDay();
+    let dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = '';
+
+    if(currentDay === 6 || currentDay === 0){
+        day = dayArray[currentDay];
+    }else{
+        day = dayArray[currentDay]
+    }
 
 
 
+    res.render("list", {kindOfDay: day})
+
+
+
+
+
+
+    
+})
 
 
 
