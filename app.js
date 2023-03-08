1b2f5947caae605530d72cd419d9bfe088bfff26
Blvckstone <mohammadshoiab9011@gmail.com>
2023-03-08 11:11:38 +0530

    C:/Program Files/Git/work route added successfully into ToDoList Project

--- app.js
+++ app.js
@@ -1,60 +1,52 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-let itemsArray = ["Reading", "Swimming", "Riding"];
+let toDoItems = ["Reading", "Swimming", "Riding"];
+let workItems = [];
 
+app.get("/", function (req, res) {
+  let day = "";
+  let today = new Date();
+  // let currentDay = today.getDay();
 
-app.get("/", function(req, res){
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-    let day = '';
-    let today = new  Date();
-    // let currentDay = today.getDay();
-    
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-    }
+  day = today.toLocaleDateString("en-US", options);
+  res.render("list", { listTitle: day, itemShow: toDoItems });
+});
 
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", itemShow: workItems });
+});
 
-    day = today.toLocaleDateString("en-US", options)
-    res.render("list", {kindOfDay: day, itemShow: itemsArray})
- 
-})
+app.post("/", function (req, res) {
+  // console.log(req.body.list)
 
+  if (req.body.list === "Work List") {
+    workItems.push(req.body.newItem);
+    res.redirect("/work");
+  } else {
+    toDoItems.push(req.body.newItem);
+    res.redirect("/");
+  }
+});
 
-app.post("/", function(req, res){
-    itemsArray.push(req.body.newItem)
-    res.redirect('/')
-})
-
-
-app.listen(3000, function(){console.log('Server Started!')})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.listen(3000, function () {
+  console.log("Server Started!");
+});
 
 // let dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
 
-    // if(currentDay === 6 || currentDay === 0){
-        // day = dayArray[currentDay];
-    // }else{
-        // day = dayArray[currentDay]
-    // }
+// if(currentDay === 6 || currentDay === 0){
+// day = dayArray[currentDay];
+// }else{
+// day = dayArray[currentDay]
+// }
